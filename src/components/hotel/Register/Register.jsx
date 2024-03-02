5ae19bf81961b84916fcff4c048bb22cb3427a9b
lakindulk <lakindulk9@gmail.com>
2024-03-03 00:57:18 +0530

    Register

--- src/components/hotel/Register/Register.jsx
+++ src/components/hotel/Register/Register.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { Tab, Tabs, Typography, Box, Button } from "@mui/material";
+import { Tab, Tabs, Typography, Box, Button, Grid } from "@mui/material";
 import PropTypes from "prop-types";
 import Customtextfield from "../Login/Customtextfield";
+import Background from "../../../assets/Register.jpg"
+import "./register.css";
+
 function a11yProps(index) {
   return {
     id: `vertical-tab-${index}`,
@@ -37,7 +40,6 @@ const tabLabels = [
   { label: "For Local Agent", index: 1 },
   { label: "For Forigen Agent", index: 2 },
   { label: "For Adviser", index: 3 },
-  
 ];
 
 function TabPanel(props) {
@@ -72,25 +74,53 @@ const Register = (props) => {
   };
   const handleChange = (event, newValue) => {
     const selectedRole = tabLabels[newValue].label;
-  setRole(selectedRole);
+    setRole(selectedRole);
     setValue(newValue);
   };
-  
+
   return (
-    <div style={{ backgroundColor: 'white', height: '100vh', margin: 0, padding: 0 }}>
-      <Tabs
+    <div
+      style={{
+        backgroundImage: `url(${Background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        height: "100vh",
+        margin: 0,
+        paddingTop:'0px',
+        paddingLeft:'7%'
+      }}
+    >
+      
+      <Grid container paddingLeft={{lg:'10%'}}  className="registerform">
+  <Grid item xs={6}>
+    <a href="/">
+    <Button variant="contained" sx={{
+    borderRadius: '30px',
+    backgroundColor: 'black',
+    boxShadow:'none',
+    '&:hover': {
+      backgroundColor: 'black', 
+      boxShadow:'none'
+    },
+  }}>Back</Button>
+    </a>
+  
+
+  <Tabs
         value={value}
         onChange={handleChange}
         aria-label="basic tabs example"
         orientation="horizontal"
         variant="scrollable"
         sx={{
+          color:'black',
           "& .Mui-selected": {
-            backgroundColor: "#FFFFFF !important",
-            color: "#47464F !important",
+            backgroundColor: "transparent !important",
+            color: "black !important",
           },
           "& .Mui-selected:hover": {
-            backgroundColor: "#FFFFFF !important",
+            backgroundColor: "transparent !important",
           },
           "& .MuiTabs-indicator": {
             backgroundColor: "#48BB78",
@@ -126,27 +156,68 @@ const Register = (props) => {
           />
         ))}
       </Tabs>
-      {role==='For Local Agent' && <Customtextfield label="NIC" marginTop="16px" marginBottom='16px'/>}
-      {role==='For Forigen Agent' &&  <Customtextfield label="Passport Number" marginTop="16px" marginBottom='16px'/>}
-      {(role === 'For Forigen Agent' || role === 'For Local Agent') && 
-      <div >
-      <input
-        type="file"
-        accept="image/*"
-        style={{ display: "none" }}
-        id="uploadImageInput"
-        onChange={handleFileChange}
-      />
-      <label htmlFor="uploadImageInput">
-        <Button component="span">Upload image of {role === 'For Local Agent' ? <>NIC Front/Back</>:<>Passport</>}</Button>
-      </label>
-      {image && <p>Selected Image: {image.name}</p>}
-    </div>
-      }
+      <Grid paddingLeft={{lg:'32px'}}>
+      {role === "For Local Agent" && (
+        <Customtextfield label="NIC" marginTop="16px" marginBottom="16px" />
+      )}
+      {role === "For Forigen Agent" && (
+        <Customtextfield
+          label="Passport Number"
+          marginTop="16px"
+          marginBottom="16px"
+        />
+      )}
+      {(role === "For Forigen Agent" || role === "For Local Agent") && (
+        <div>
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            id="uploadImageInput"
+            onChange={handleFileChange}
+          />
+          <label htmlFor="uploadImageInput">
+            <Button sx={{
+   
+    color:'black',
+    
+    '&:hover': {
+     
+      color:'black',
+      
+    },}} component="span">
+              Upload image of{" "}
+              {role === "For Local Agent" ? <>NIC Front/Back</> : <>Passport</>}
+            </Button>
+          </label>
+          {image && <p>Selected Image: {image.name}</p>}
+        </div>
+      )}
       <Customtextfield label="Name" marginTop="16px" />
       <Customtextfield label="Contact Number" marginTop="16px" />
       <Customtextfield label="Email" marginTop="16px" />
       <Customtextfield label="Password" marginTop="16px" />
+      <Button variant="outlined" sx={{
+    borderRadius: '30px',
+    borderColor: 'black',
+    boxShadow:'none',
+    width:'93%',
+    color:'black',
+    marginTop:'32px',
+    height:'48px',
+    '&:hover': {
+      backgroundColor: 'black', 
+      color:'white',
+      borderColor:'black',
+      boxShadow:'none'
+    },}}>Register</Button>
+      </Grid> </Grid>
+  <Grid item xs={6} >
+    <Box width={{lg:'90%'}}  >
+
+    </Box>
+s  </Grid>
+</Grid>
       
     </div>
   );
