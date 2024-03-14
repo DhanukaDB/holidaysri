bc01b34843e2b1b7151702a7d61d4332b2facda9
Yasara19 <86340688+Yasara19@users.noreply.github.com>
2024-03-14 20:22:54 +0530

    fetch all events and locations

--- src/pages/alllocations/Locations.jsx
+++ src/pages/alllocations/Locations.jsx
@@ -1,45 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Box, Button, Typography, Modal, Grid } from "@mui/material";
-import Customtextfield from "../../components/hotel/Login/Customtextfield";
+import { Box, Typography, Grid } from "@mui/material";
 import axios from "axios";
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: { lg: 530, xs: 250 },
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  borderRadius: "8px",
-  p: 4,
-};
+import { Link } from "react-router-dom";
+
 const Locations = (props) => {
-  const [openlocation, setOpenlocation] = React.useState(false);
-  const handleOpenlocation = () => setOpenlocation(true);
-  const handleCloselocation = () => setOpenlocation(false);
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  const [images, setImages] = useState([]);
-  const [locationName, setLocationName] = useState("");
-  const [district, setDistrict] = useState("");
-  const [province, setProvince] = useState("");
-  const [distanceFromColombo, setDistanceFromColombo] = useState("");
-  const [details, setDetails] = useState("");
   const [location, setLocation] = useState([]);
-  const [eventLocation, setEventLocation] = useState("");
-  const [eventName, setEventName] = useState("");
-  const [description, setDescription] = useState("");
-  const [event, setEvent] = useState([]);
 
   useEffect(() => {
     async function getAllLocations() {
       try {
-        const res = await axios.get(
-          "http://localhost:8000/location/"
-        );
+        const res = await axios.get("http://localhost:8000/location/");
         setLocation(res.data);
-        console.log(res.data);
       } catch (error) {
         console.error("Error fetching Locations:", error);
         alert("Error fetching Locations: " + error.message);
@@ -47,7 +18,6 @@ const Locations = (props) => {
     }
     getAllLocations();
   }, []);
-  //http://localhost:8000/location/
 
   return (
     <div
@@ -81,70 +51,69 @@ const Locations = (props) => {
           </Typography>
         </Box>
 
-        <Grid container>
-          <Box
-            sx={{
-              width: { lg: "1100px", xs: "300px" },
-            }}
-          >
-            <Typography
-              fontSize={{ lg: "24px", xs: "22px" }}
-              sx={{ color: "white", fontFamily: "mansalva" }}
-              marginTop="16px"
-            >
-              LOCATIONS:
-            </Typography>
-          </Box>
-          <>
-            <Grid container spacing={2}>
-              {location.map((location) => (
-                <Grid item xs={6} lg={3}>
+        <Grid container spacing={2}>
+          {location.map((location) => (
+            <Grid item xs={6} lg={3} key={location._id}>
+              <Link to={`/destination/${location._id}`} style={{ textDecoration: 'none' }}>
+                <Box
+                  border={3}
+                  sx={{
+                    width: "100%",
+                    height: "250px",
+                    borderColor: "black",
+                    borderRadius: "30px",
+                    backgroundColor: "rgba(48, 103, 84, 0.5)",
+                    position: "relative",
+                    overflow: "hidden",
+                    cursor: "pointer",
+                  }}
+                >
+                  <ImageSlider images={location.images} />
                   <Box
-                    key={location._id}
-                    border={3}
                     sx={{
-                      width: { lg: "100%", xs: "100%" },
-                      height: { lg: "250px" },
-                      borderColor: "black",
-                      borderRadius: "30px",
-                      backgroundColor: "rgba(48, 103, 84, 0.5)",
-                      padding: "20%",
-                      marginTop: { lg: "16px", xs: "16px" },
+                      position: "absolute",
+                      bottom: 0,
+                      left: 0,
+                      width: "100%",
+                      textAlign: "center",
+                      color: "white",
+                      p: 2,
                     }}
                   >
-                    <Grid container spacing={2}>
-                      <Grid item xs={12} lg={12}>
-                        <Typography
-                          sx={{
-                            color: "white",
-                            fontWeight: "400",
-                            textAlign: "center",
-                            fontSize: { lg: "42px", xs: "20px" },
-                          }}
-                        >
-                          {location.locationName}
-                        </Typography>
-                        <Typography
-                          sx={{
-                            color: "white",
-                            fontWeight: "400",
-                            textAlign: "center",
-                            fontSize: { lg: "16px", xs: "16px" },
-                            marginTop: "8px",
-                          }}
-                        >
-                          {location.district} District
-                        </Typography>
-                      </Grid>
-                    </Grid>
+                    <Typography variant="h6">{location.locationName}</Typography>
+                    <Typography variant="body1">
+                      {location.district} District
+                    </Typography>
                   </Box>
-                </Grid>
-              ))}{" "}
+                </Box>
+              </Link>
             </Grid>
-          </>
+          ))}
         </Grid>
       </Box>
     </div>
+  );
+};
+
+const ImageSlider = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(intervalId);
+  }, [images]);
+
+  return (
+    <img
+      src={images[currentImageIndex]}
+      alt={`Location Image ${currentImageIndex + 1}`}
+      style={{ width: "100%", height: "100%", objectFit: "cover" }}
+    />
   );
 };
 
