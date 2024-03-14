0f4f282488956c4fea9511b2c0099d6ac6005905
Kavindu_J22 <it21032806@my.sliit.lk>
2024-03-15 01:30:56 +0530

    update gallery

--- src/components/hotel/mailList/Galary.jsx
+++ src/components/hotel/mailList/Galary.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import img1 from '../../../assets/img1.jpg'
+import img1 from '../../../assets/gimg1.jpg'
+import img2 from '../../../assets/gimg2.jpg'
+import img3 from '../../../assets/gimg3.jpg'
+import img4 from '../../../assets/gimg4.jpg'
+import img5 from '../../../assets/gimg5.jpg'
+import AOS from 'aos';
 
 function GallerySection() {
+  AOS.init({ duration: 1000, once: false});
   return (
     <section className="gallery" id="gallery" data-aos="fade">
       <div className="container">
@@ -13,12 +19,12 @@ function GallerySection() {
         <ul className="gallery-list">
           <li className="gallery-item" data-aos="slide-right">
             <figure className="gallery-image">
-              <img src={img1} alt="Gallery image" />
+              <img src={img2} alt="Gallery image" />
             </figure>
           </li>
           <li className="gallery-item" data-aos="slide-left">
             <figure className="gallery-image">
-            <img src={img1} alt="Gallery image" />
+            <img src={img3} alt="Gallery image" />
             </figure>
           </li>
           <li className="gallery-item" data-aos="slide-up">
@@ -28,12 +34,12 @@ function GallerySection() {
           </li>
           <li className="gallery-item" data-aos="slide-right">
             <figure className="gallery-image">
-            <img src={img1} alt="Gallery image" />
+            <img src={img4} alt="Gallery image" />
             </figure>
           </li>
           <li className="gallery-item" data-aos="slide-left">
             <figure className="gallery-image">
-            <img src={img1} alt="Gallery image" />
+            <img src={img5} alt="Gallery image" />
             </figure>
           </li>
         </ul>
