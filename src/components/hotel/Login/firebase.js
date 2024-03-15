9e61756a3877c639b30abdc02f876d33b3845078
Dhanuka Balasooriya <it20207236@my.sliit.lk>
2024-03-15 12:27:32 +0530

    Google signin Not complete

---
+++ src/components/hotel/Login/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAtjyfSXQWgYZhVkX3J6IWF_5SlXRnnfbk",
+  authDomain: "holidaysri-33f46.firebaseapp.com",
+  projectId: "holidaysri-33f46",
+  storageBucket: "holidaysri-33f46.appspot.com",
+  messagingSenderId: "427937843625",
+  appId: "1:427937843625:web:7effdc34df8d935dab5cb8",
+  measurementId: "G-DK5LF9V3SK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth);
